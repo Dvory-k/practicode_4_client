8804438212088b011ce4e5a0e4c38f1c0e86f6a7
Dvory <kl5837789@gmail.com>
2025-03-18 17:19:31 +0200

    problem5

--- src/service.js
+++ src/service.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-// import axios from './axiosConfig.ts'
+// import axios from 'axios';
+import axios from './axiosConfig.ts'
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
