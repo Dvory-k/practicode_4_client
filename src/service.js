94f09aa5a8c2b8bc8728466814640eab098293f6
Dvory <kl5837789@gmail.com>
2025-03-18 14:51:58 +0200

    123

--- src/service.js
+++ src/service.js
@@ -1,7 +1,7 @@
 // import axios from 'axios';
 import axios from './axiosConfig.ts'
 
-axios.defaults.baseURL = process.env.NODE_ENV;
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 // const apiUrl = "https://localhost:5256"
 
