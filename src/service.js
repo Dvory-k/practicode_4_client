1f450028a8d96f32a90035b5989bd9820dd67871
Dvory <kl5837789@gmail.com>
2025-03-18 14:10:52 +0200

    connect to server1

--- src/service.js
+++ src/service.js
@@ -7,7 +7,8 @@ axios.defaults.baseURL = process.env.NODE_ENV;
 
 export default {
   getTasks: async () => {
-    const result = await axios.get(`items`)
+    const result = await axios.get(`items`).then(succ=>
+      console.log(succ)).catch(err=>console.log(err))
       
     return result.data;
   },
